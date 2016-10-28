4d470200ba0e712c668c866f7135aeaffedd0c82
Gabi Dobocan <gabi@telepat.io>
2016-10-28 03:49:15 +0300

    Add comments, fix lint issues, extract global constants

--- airflow/contrib/executors/astronomer_mesos_executor.py
+++ airflow/contrib/executors/astronomer_mesos_executor.py
@@ -1,10 +1,11 @@
+"""Astronomer Airflow executor for Mesos"""
 #!/usr/bin/env python
-
-import os
-from threading import Lock
 
 from future import standard_library
 standard_library.install_aliases()
+import os
+from threading import Lock
+
 from builtins import str
 from datetime import datetime
 from itertools import count
@@ -25,12 +26,31 @@ _log = logging.getLogger(__name__)
 DEFAULT_FRAMEWORK_NAME = 'Airflow'
 FRAMEWORK_CONNID_PREFIX = 'mesos_framework_'
 
+DEFAULT_TASK_CPU = 1
+DEFAULT_TASK_MEMORY = 256
+
+# Tasks are reconciliated using an exponential backoff algorithm.
+# A task is marked as old if it has not been updated since 2**backoff_iteration seconds
+
+# The backoff level a task should have when switching from clean to dirty
+# This applies to not-yet-running tasks that have not been updated for a while
+BACKOFF_ITERATION_AFTER_DIRTY = 2
+# The backoff level a task should have when switching from dirty to clean
+BACKOFF_ITERATION_AFTER_CLEAN = 5
+# The maximum backoff iteration level. Tasks that are still dirty get marked for termination here
+BACKOFF_ITERATION_MAX = 8
+# The initial backoff iteration level for fresh tasks
+BACKOFF_ITERATION_INITIAL = 1
+
+
 def copy_env_var(command, env_var_name):
+    """Copies an environment variable to a Mesos Command"""
     env_var = command.environment.variables.add()
     env_var.name = env_var_name
     env_var.value = os.getenv(env_var_name, '')
 
 class Task(object):
+    """Encapsulates data about a specific Mesos task"""
     _ids = count(0)
 
     def __init__(self, **kwargs):
@@ -41,11 +61,10 @@ class Task(object):
         self.cmd = kwargs.get("cmd") or ""
         self.task_id = str(self._ids.next())
         self.agent_id = kwargs.get("agent_id") or None
-        
-        self.cpus = configuration.getint('mesos', 'TASK_CPU') or 1
-        self.mem = configuration.getint('mesos', 'TASK_MEMORY') or 256
+        self.cpus = configuration.getint('mesos', 'TASK_CPU') or DEFAULT_TASK_CPU
+        self.mem = configuration.getint('mesos', 'TASK_MEMORY') or DEFAULT_TASK_MEMORY
         self.clean = True
-        self.backoff_iteration = 1
+        self.backoff_iteration = BACKOFF_ITERATION_INITIAL
 
     def __repr__(self):
         return "Task({} {})".format(self.task_id, self.key)
@@ -54,6 +73,7 @@ class Task(object):
         return self.__repr__()
 
     def as_mesos_task(self, agent_id):
+        """Returns a Mesos Task representation of the task"""
         task = mesos_pb2.TaskInfo()
         task.task_id.value = self.task_id
         task.slave_id.value = agent_id
@@ -101,23 +121,28 @@ class Task(object):
         return task
 
     def failed(self):
+        """Is the current task in a failed state?"""
         return self.state in (mesos_pb2.TASK_LOST,
                               mesos_pb2.TASK_ERROR,
                               mesos_pb2.TASK_KILLED,
                               mesos_pb2.TASK_FAILED)
 
     def finished(self):
+        """Has the current task succesfully finished?"""
         return self.state == mesos_pb2.TASK_FINISHED
 
     def running(self):
+        """Is the task in a running state?"""
         return self.state in (mesos_pb2.TASK_STAGING,
                               mesos_pb2.TASK_STARTING,
                               mesos_pb2.TASK_RUNNING)
 
     def old(self):
-        return ((datetime.utcnow() - self.updated).total_seconds() > 2**self.backoff_iteration)
+        """Returns True if the task's last update is older than the current backoff timeout."""
+        return not self.state == mesos_pb2.TASK_RUNNING and ((datetime.utcnow() - self.updated).total_seconds() > 2**self.backoff_iteration)
 
     def update(self, update):
+        """Update the task's status"""
         _log.info("Updating status of %s from %s to %s",
                   self,
                   mesos_pb2.TaskState.Name(self.state),
@@ -127,46 +152,36 @@ class Task(object):
         self.make_clean()
 
     def touch(self):
+        """Set the task's last updated time to now"""
         self.updated = datetime.utcnow()
 
     def make_dirty(self):
+        """Make the task dirty and subject to reconciliation"""
         if self.clean:
-            self.backoff_iteration = 2
+            self.backoff_iteration = BACKOFF_ITERATION_AFTER_DIRTY
         else:
             self.backoff_iteration += 1
 
-        if self.backoff_iteration > 8:
+        if self.backoff_iteration > BACKOFF_ITERATION_MAX:
             self.task_id = None
 
         self.clean = False
 
     def make_clean(self):
+        """Mark the task as clean"""
         self.clean = True
-        self.backoff_iteration = 5
+        self.backoff_iteration = BACKOFF_ITERATION_AFTER_CLEAN
 
     def get_task_status(self):
+        """Returns a Mesos TaskStatus representation of the task"""
         task_status = mesos_pb2.TaskStatus()
         task_status.slave_id.value = self.agent_id
         task_status.task_id.value = self.task_id
         task_status.state = self.state
         return task_status
 
-    def to_dict(self):
-        task_dict = {
-            "task_id": self.task_id,
-            "state": self.state,
-            "created": self.created,
-            "updated": self.updated
-        }
-        return task_dict
-
-    @classmethod
-    def from_dict(cls, task_dict):
-        task = cls(**task_dict)
-        task.clean = not task.running()
-        return task
-
 class AirflowMesosScheduler(mesos.interface.Scheduler):
+    """Astronomer Mesos Scheduler for the Airflow executor"""
     def __init__(self, task_queue, task_mutex, result_queue, result_mutex):
         self.task_queue = task_queue
         self.task_mutex = task_mutex
@@ -179,21 +194,25 @@ class AirflowMesosScheduler(mesos.interface.Scheduler):
         return "AirflowMesosScheduler"
 
     def _task(self, taskId):
+        """Returns a task with a specific task id"""
         return self.tasks.get(taskId)
 
     def makeOldTasksDirty(self, driver):
+        """Mark all stale tasks as subject for reconciliation"""
         for task in self.tasks.itervalues():
-                if not task.state == mesos_pb2.TASK_RUNNING and task.old():
-                    task.make_dirty()
+            if task.old():
+                task.make_dirty()
         self.triggerResync(driver)
 
     def makeTasksDirty(self, driver, agentId=None):
+        """Mark tasks as subject for reconciliation - optionally specify an agent id"""
         for task in self.tasks.itervalues():
-                if task.running() and (not agentId or task.agent_id == agentId):
-                    task.make_dirty()
+            if task.running() and (not agentId or task.agent_id == agentId):
+                task.make_dirty()
         self.triggerResync(driver)
 
     def triggerResync(self, driver):
+        """Trigger a reconciliation for dirty tasks"""
         unclean_task_statuses = [task.get_task_status() for task in self.tasks.itervalues() if not task.clean]
 
         if unclean_task_statuses:
@@ -202,6 +221,7 @@ class AirflowMesosScheduler(mesos.interface.Scheduler):
             driver.reconcileTasks(unclean_task_statuses)
 
     def registered(self, driver, frameworkId, masterInfo):
+        """Called when the Mesos framework is registered"""
         _log.info("AirflowMesosScheduler registered to mesos with framework ID %s", frameworkId.value)
 
         if configuration.getboolean('mesos', 'CHECKPOINT') and configuration.get('mesos', 'FAILOVER_TIMEOUT'):
@@ -223,63 +243,80 @@ class AirflowMesosScheduler(mesos.interface.Scheduler):
             Session.remove()
 
     def reregistered(self, driver, masterInfo):
+        """Called when the Mesos framework is reregistered"""
         _log.info("AirflowScheduler re-registered to mesos")
+        # Trigger an all-out reconciliation
         self.makeTasksDirty(driver)
 
     def disconnected(self, driver):
+        """Called when the Mesos framework is disconnected from Mesos"""
+        # TODO Should we just abort here, if no checkpointing?
         _log.info("AirflowScheduler disconnected from mesos")
 
     def offerRescinded(self, driver, offerId):
+        """Called when a previous resource offer gets called back"""
         _log.info("AirflowScheduler offer %s rescinded", str(offerId))
+        # Reconciliate all tasks, just in case we accepted the offer and we miss TASK_LOST
         self.makeTasksDirty(driver)
 
     def frameworkMessage(self, driver, executorId, slaveId, message):
+        """Called when a framework message is received"""
         _log.info("AirflowScheduler received framework message %s", message)
 
     def executorLost(self, driver, executorId, slaveId, status):
+        """Called when the Mesos executor is lost"""
+        # TODO Not sure how to best handle this.
         _log.warning("AirflowScheduler executor %s lost", str(executorId))
 
     def slaveLost(self, driver, slaveId):
+        """Called when a slave is disconnected from Mesos"""
         _log.warning("AirflowScheduler slave %s lost", str(slaveId))
+        # Mark all tasks running on the slave as dirty
         self.makeTasksDirty(driver, slaveId)
 
     def error(self, driver, message):
+        """Called by Mesos on a framework error"""
         _log.error("AirflowScheduler driver aborted %s", message)
         raise AirflowException("AirflowScheduler driver aborted %s" % message)
         #os._exit(1)
 
     def resourceOffers(self, driver, offers):
+        """Called by Mesos with resource offers"""
+        # If there's neved been a reconciliation done, trigger one
         if self.syncd is None:
             _log.debug("Scheduler is not yet sync'd - triggering a resync")
             self.makeTasksDirty(driver)
 
+        # Scan for stale tasks, and trigger a reconciliation if needed
         self.makeOldTasksDirty(driver)
 
         for offer in offers:
-            offerCpus = 0
-            offerMem = 0
+            # Gather total offer resources
+            offer_cpus = 0
+            offer_mem = 0
             for resource in offer.resources:
                 if resource.name == "cpus":
-                    offerCpus += resource.scalar.value
+                    offer_cpus += resource.scalar.value
                 elif resource.name == "mem":
-                    offerMem += resource.scalar.value
+                    offer_mem += resource.scalar.value
 
-            _log.info("Received offer from %s with cpus: %s and mem: %s", offer.slave_id.value[-8:], offerCpus, offerMem)
+            _log.info("Received offer from %s with cpus: %s and mem: %s", offer.slave_id.value[-8:], offer_cpus, offer_mem)
 
             tasks = []
             i = 0
 
+            # Aquire lock for reading the task queue
             self.task_mutex.acquire()
             try:
-                while offerCpus > 0 and offerMem > 0 and i < len(self.task_queue):
+                while offer_cpus > 0 and offer_mem > 0 and i < len(self.task_queue):
                     task = self.task_queue[i]
-                    if task.cpus <= offerCpus and task.mem <= offerMem:
+                    if task.cpus <= offer_cpus and task.mem <= offer_mem:
                         self.task_queue.pop(i)
-                        task.agent_id = offer.slave_id.value;
+                        task.agent_id = offer.slave_id.value
                         tasks.append(task)
                         self.tasks[task.task_id] = task
-                        offerCpus -= task.cpus
-                        offerMem -= task.mem
+                        offer_cpus -= task.cpus
+                        offer_mem -= task.mem
                     i += 1
             finally:
                 self.task_mutex.release()
@@ -290,22 +327,28 @@ class AirflowMesosScheduler(mesos.interface.Scheduler):
                 continue
 
             _log.info("Launching Tasks %s", tasks)
-            
+
             operations = []
             for task in tasks:
+                # Set the task update date to the moment of launch
                 task.touch()
                 mesos_task = task.as_mesos_task(offer.slave_id.value)
                 operation = mesos_pb2.Offer.Operation()
                 operation.launch.task_infos.extend([mesos_task])
                 operation.type = mesos_pb2.Offer.Operation.LAUNCH
                 operations.append(operation)
-            
+
             driver.acceptOffers([offer.id], operations)
 
     def statusUpdate(self, driver, update):
+        """Called by Mesos with task status updates"""
+        # Retrieve task information
         task = self._task(update.task_id.value)
+
+        # Is the task unknown?
         if not task or task.task_id != update.task_id.value:
             if update.state == mesos_pb2.TASK_RUNNING:
+                # Kill of running, unrecognized tasks
                 _log.info("Killing unrecognized task: %s", update.task_id.value)
                 driver.killTask(update.task_id)
             else:
@@ -319,12 +362,14 @@ class AirflowMesosScheduler(mesos.interface.Scheduler):
 
         task.update(update)
 
+        # Is the task finished or failed?
         if not task.running():
+            # Aquire a lock for writing to the result queue
             self.result_mutex.acquire()
             try:
                 if task.failed():
                     _log.error("\t%s is in failed state %s with message '%s'",
-                                task, mesos_pb2.TaskState.Name(update.state), update.message)
+                               task, mesos_pb2.TaskState.Name(update.state), update.message)
                     _log.error("\tData:  %s", repr(str(update.data)))
                     _log.error("\tSent by: %s", mesos_pb2.TaskStatus.Source.Name(update.source))
                     _log.error("\tReason: %s", mesos_pb2.TaskStatus.Reason.Name(update.reason))
@@ -337,16 +382,21 @@ class AirflowMesosScheduler(mesos.interface.Scheduler):
             finally:
                 self.result_mutex.release()
 
+            # Remove ended tasks from the current state
             del self.tasks[task.task_id]
 
+        # If reconciliating, and all tasks are clean, we're done
         if not self.syncd and all(task.clean for task in self.tasks.values()):
             _log.debug("Agent is now sync'd - perform full reconcile to tidy up old tasks")
             self.syncd = True
+            # Trigger a final implicit reconciliation, to fix potential lost tasks we don't know
             driver.reconcileTasks([])
 
 class AstronomerMesosExecutor(BaseExecutor):
-
+    """Airflow executor that schedules tasks on Mesos"""
     def start(self):
+        """Called on executor launch"""
+        # Make sure we have all needed configuration
         if not configuration.get('mesos', 'MASTER'):
             logging.error("Expecting mesos master URL for mesos executor")
             raise AirflowException("mesos.master not provided for mesos executor")
@@ -358,9 +408,13 @@ class AstronomerMesosExecutor(BaseExecutor):
                 logging.error("Expecting authentication secret in the environment")
                 raise AirflowException("mesos.default_secret not provided in authenticated mode")
 
+        # FIFO task list
         self.task_queue = []
+        # Lock for the task list
         self.task_mutex = Lock()
+        # List for task results
         self.result_queue = []
+        # Lock for the results list
         self.result_mutex = Lock()
 
         framework = mesos_pb2.FrameworkInfo()
@@ -415,6 +469,8 @@ class AstronomerMesosExecutor(BaseExecutor):
         self.mesos_driver.start()
 
     def execute_async(self, key, command, queue=None):
+        """Called by Airflow to schedule tasks for execution"""
+        # Aquire lock for writing to the task queue
         self.task_mutex.acquire()
         try:
             self.task_queue.append(Task(key=key, cmd=command))
@@ -422,6 +478,8 @@ class AstronomerMesosExecutor(BaseExecutor):
             self.task_mutex.release()
 
     def sync(self):
+        """Called by Airflow to retrieve a batch of task results"""
+        # Aquire lock for reading from the results queue
         self.result_mutex.acquire()
         try:
             while self.result_queue:
@@ -431,4 +489,5 @@ class AstronomerMesosExecutor(BaseExecutor):
             self.result_mutex.release()
 
     def end(self):
+        """Called by Airflow to end execution"""
         self.mesos_driver.stop()
