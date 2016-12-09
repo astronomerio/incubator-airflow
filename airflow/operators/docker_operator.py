911119da4b3e704982f1f16ab1afb2917908a35e
Mike Perry <mike@astronomer.io>
2016-12-09 16:55:46 -0500

    now accepting a param to remove containers on exit in DockerOperator

--- airflow/operators/docker_operator.py
+++ airflow/operators/docker_operator.py
@@ -32,6 +32,8 @@ class DockerOperator(BaseOperator):
     :type environment: dict
     :param force_pull: Pull the docker image on every run.
     :type force_pull: bool
+    :param remove: Remove the container when stopped or killed
+    :type remove: bool
     :param mem_limit: Maximum amount of memory the container can use. Either a float value, which
         represents the limit in bytes, or a string like ``128m`` or ``1g``.
     :type mem_limit: float or str
@@ -77,6 +79,7 @@ class DockerOperator(BaseOperator):
             docker_url='unix://var/run/docker.sock',
             environment=None,
             force_pull=False,
+            remove=False,
             mem_limit=None,
             network_mode=None,
             privileged=False,
@@ -100,6 +103,7 @@ class DockerOperator(BaseOperator):
         self.docker_url = docker_url
         self.environment = environment or {}
         self.force_pull = force_pull
+        self.remove = remove
         self.image = image
         self.mem_limit = mem_limit
         self.network_mode = network_mode
@@ -172,6 +176,9 @@ class DockerOperator(BaseOperator):
             if exit_code != 0:
                 raise AirflowException('docker container failed')
 
+            # remove the container if specified in initializer
+            self.remove()
+
             if self.xcom_push:
                 return self.cli.logs(container=self.container['Id']) if self.xcom_all else str(line.strip())
 
@@ -186,3 +193,9 @@ class DockerOperator(BaseOperator):
         if self.cli is not None:
             logging.info('Stopping docker container')
             self.cli.stop(self.container['Id'])
+            self.remove()
+
+    def remove(self):
+        if self.remove:
+            self.cli.remove_container(self.container['Id'])
+
