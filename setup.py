a996b550599a67ee59ff5c400bf716fb2f044c9b
Ben <benjaminrgregory@gmail.com>
2018-12-05 14:52:25 -0700

    Add <3 to redis version

--- setup.py
+++ setup.py
@@ -172,7 +172,7 @@ password = [
 github_enterprise = ['Flask-OAuthlib>=0.9.1']
 qds = ['qds-sdk>=1.9.6']
 cloudant = ['cloudant>=0.5.9,<2.0'] # major update coming soon, clamp to 0.x
-redis = ['redis>=2.10.5']
+redis = ['redis>=2.10.5,<3']
 
 all_dbs = postgres + mysql + hive + mssql + hdfs + vertica + cloudant
 devel = [
