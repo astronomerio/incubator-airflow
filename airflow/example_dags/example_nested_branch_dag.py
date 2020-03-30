apache/spark

[AIRFLOW-4453] Make behavior of `none_failed` consistent with documentation (#7464)
The documentation for the `none_failed` trigger rule
(https://airflow.apache.org/docs/stable/concepts.html#trigger-rules)
describes its behavior as "all parents have not failed (`failed` or
`upstream_failed`) i.e. all parents have succeeded or been skipped."
With that definition in mind, there is no reason that the check for
`none_failed` should ever have to check for skipped upstream tasks or
set the current task to `skipped`. The current behavior causes the rule
to skip if all upstream tasks have skipped, which is more than a little
confusing. This fixes the behavior to be consistent with the documentation.

Co-authored-by: root <jcroteau@liveramp.com>

airflow/example_dags/example_nested_branch_dag.py
@@ -1,0 +1,42 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+Example DAG demonstrating a workflow with nested branching. The join tasks are created with
+``none_failed_or_skipped`` trigger rule such that they are skipped whenever their corresponding
+``BranchPythonOperator`` are skipped.
+"""
+
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.utils.dates import days_ago
+
+with DAG(dag_id="example_nested_branch_dag", start_date=days_ago(2), schedule_interval="@daily") as dag:
+    branch_1 = BranchPythonOperator(task_id="branch_1", python_callable=lambda: "true_1")
+    join_1 = DummyOperator(task_id="join_1", trigger_rule="none_failed_or_skipped")
+    true_1 = DummyOperator(task_id="true_1")
+    false_1 = DummyOperator(task_id="false_1")
+    branch_2 = BranchPythonOperator(task_id="branch_2", python_callable=lambda: "true_2")
+    join_2 = DummyOperator(task_id="join_2", trigger_rule="none_failed_or_skipped")
+    true_2 = DummyOperator(task_id="true_2")
+    false_2 = DummyOperator(task_id="false_2")
+    false_3 = DummyOperator(task_id="false_3")
+
+    branch_1 >> true_1 >> join_1
+    branch_1 >> false_1 >> branch_2 >> [true_2, false_2] >> join_2 >> false_3 >> join_1

airflow/ti_deps/deps/trigger_rule_dep.py
@@ -143,6 +143,9 @@ class TriggerRuleDep(BaseTIDep):
             elif trigger_rule == TR.NONE_FAILED:
                 if upstream_failed or failed:
                     ti.set_state(State.UPSTREAM_FAILED, session)
+            elif trigger_rule == TR.NONE_FAILED_OR_SKIPPED:
+                if upstream_failed or failed:
+                    ti.set_state(State.UPSTREAM_FAILED, session)
                 elif skipped == upstream:
                     ti.set_state(State.SKIPPED, session)
             elif trigger_rule == TR.NONE_SKIPPED:
@@ -191,6 +194,15 @@ class TriggerRuleDep(BaseTIDep):
                     .format(trigger_rule, upstream_done, upstream_tasks_state,
                             task.upstream_task_ids))
         elif trigger_rule == TR.NONE_FAILED:
+            num_failures = upstream - successes - skipped
+            if num_failures > 0:
+                yield self._failing_status(
+                    reason="Task's trigger rule '{0}' requires all upstream "
+                    "tasks to have succeeded or been skipped, but found {1} non-success(es). "
+                    "upstream_tasks_state={2}, upstream_task_ids={3}"
+                    .format(trigger_rule, num_failures, upstream_tasks_state,
+                            task.upstream_task_ids))
+        elif trigger_rule == TR.NONE_FAILED_OR_SKIPPED:
             num_failures = upstream - successes - skipped
             if num_failures > 0:
                 yield self._failing_status(

astronomerio/incubator-airflow

(cherry picked from commit f0c65e32103fa0fe91a4890d75ba31a89aa226e7)

airflow/example_dags/example_nested_branch_dag.py
@@ -1,0 +1,42 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+Example DAG demonstrating a workflow with nested branching. The join tasks are created with
+``none_failed_or_skipped`` trigger rule such that they are skipped whenever their corresponding
+``BranchPythonOperator`` are skipped.
+"""
+
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.utils.dates import days_ago
+
+with DAG(dag_id="example_nested_branch_dag", start_date=days_ago(2), schedule_interval="@daily") as dag:
+    branch_1 = BranchPythonOperator(task_id="branch_1", python_callable=lambda: "true_1")
+    join_1 = DummyOperator(task_id="join_1", trigger_rule="none_failed_or_skipped")
+    true_1 = DummyOperator(task_id="true_1")
+    false_1 = DummyOperator(task_id="false_1")
+    branch_2 = BranchPythonOperator(task_id="branch_2", python_callable=lambda: "true_2")
+    join_2 = DummyOperator(task_id="join_2", trigger_rule="none_failed_or_skipped")
+    true_2 = DummyOperator(task_id="true_2")
+    false_2 = DummyOperator(task_id="false_2")
+    false_3 = DummyOperator(task_id="false_3")
+
+    branch_1 >> true_1 >> join_1
+    branch_1 >> false_1 >> branch_2 >> [true_2, false_2] >> join_2 >> false_3 >> join_1

airflow/ti_deps/deps/trigger_rule_dep.py
@@ -155,6 +155,9 @@ class TriggerRuleDep(BaseTIDep):
             elif tr == TR.NONE_FAILED:
                 if upstream_failed or failed:
                     ti.set_state(State.UPSTREAM_FAILED, session)
+            elif tr == TR.NONE_FAILED_OR_SKIPPED:
+                if upstream_failed or failed:
+                    ti.set_state(State.UPSTREAM_FAILED, session)
                 elif skipped == upstream:
                     ti.set_state(State.SKIPPED, session)
             elif tr == TR.NONE_SKIPPED:
@@ -203,6 +206,15 @@ class TriggerRuleDep(BaseTIDep):
                     .format(tr, upstream_done, upstream_tasks_state,
                             task.upstream_task_ids))
         elif tr == TR.NONE_FAILED:
+            num_failures = upstream - successes - skipped
+            if num_failures > 0:
+                yield self._failing_status(
+                    reason="Task's trigger rule '{0}' requires all upstream "
+                    "tasks to have succeeded or been skipped, but found {1} non-success(es). "
+                    "upstream_tasks_state={2}, upstream_task_ids={3}"
+                    .format(tr, num_failures, upstream_tasks_state,
+                            task.upstream_task_ids))
+        elif tr == TR.NONE_FAILED_OR_SKIPPED:
             num_failures = upstream - successes - skipped
             if num_failures > 0:
                 yield self._failing_status(