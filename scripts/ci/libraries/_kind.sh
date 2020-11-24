apache/spark

Update to new helm stable repo (#12137)
Switch out deprecated helm repo for new stable repo.

- https://www.cncf.io/blog/2020/11/05/helm-chart-repository-deprecation-update/
- https://helm.sh/docs/faq/#i-am-getting-a-warning-about-unable-to-get-an-update-from-the-stable-chart-repository

scripts/ci/libraries/_kind.sh
@@ -338,7 +338,7 @@ function kind::deploy_airflow_with_helm() {
     kubectl create namespace "${HELM_AIRFLOW_NAMESPACE}"
     kubectl create namespace "test-namespace"
     pushd "${AIRFLOW_SOURCES}/chart" || exit 1
-    helm repo add stable https://kubernetes-charts.storage.googleapis.com
+    helm repo add stable https://charts.helm.sh/stable/
     helm dep update
     helm install airflow . --namespace "${HELM_AIRFLOW_NAMESPACE}" \
         --set "defaultAirflowRepository=${DOCKERHUB_USER}/${DOCKERHUB_REPO}" \

astronomerio/incubator-airflow

(cherry picked from commit 128c9918b5f79cb46a563b77e803c29548c4319c)
(cherry picked from commit 868ff46caffd8df26da8c4edb53e83d802b3848b)
(cherry picked from commit 005a615afd43a431a626f76076550946051ed606)

scripts/ci/libraries/_kind.sh
@@ -296,7 +296,7 @@ function deploy_airflow_with_helm() {
     verbose_kubectl create namespace "${HELM_AIRFLOW_NAMESPACE}"
     verbose_kubectl create namespace "test-namespace"
     pushd "${AIRFLOW_SOURCES}/chart" || exit 1
-    verbose_helm repo add stable https://kubernetes-charts.storage.googleapis.com
+    verbose_helm repo add stable https://charts.helm.sh/stable/
     verbose_helm dep update
     verbose_helm install airflow . --namespace "${HELM_AIRFLOW_NAMESPACE}" \
         --set "defaultAirflowRepository=${DOCKERHUB_USER}/${DOCKERHUB_REPO}" \